1aa0a9f22fb670d53b41a34cfcd97eaa0d8234ed
Alehandro26 <need26@yandex.ru>
2023-07-11 15:44:58 +0300

    Правки

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/map-yandex/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
