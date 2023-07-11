9e6018af37fb9ece15bc9bb7157930c461e0cc11
Alehandro26 <need26@yandex.ru>
2023-07-11 15:00:24 +0300

    Убрал vuex

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,11 @@
 </template>
 
 <script>
-import MainPage from '@/pages/MainPage/MainPage.vue'
-import lessVars from '@/styles/vars.less?inline';
+import MainPage from '@/pages/MainPage/MainPage.vue';
 
 export default {
   components: {
     MainPage
-  },
-  created() {
-    this.onWindowResize();
-    window.addEventListener('resize', this.onWindowResize);
-  },
-  unmounted() {
-    window.removeEventListener('resize', this.onWindowResize);
-  },
-  methods: {
-    onWindowResize() {
-      this.$store.commit('setLessVars', lessVars);
-    },
   },
 };
 </script>
